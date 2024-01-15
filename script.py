24f97d2cb9661221f9a31756627ec52aa8fa1454
RayenF <rayen.frih.pro@gmail.com>
2024-01-15 17:23:23 +0100

    Ajout du script Python

---
+++ script.py
@@ -0,0 +1,11 @@
+# script.py
+
+def addition(a, b):
+    result = a + b
+    return result
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 7
+    sum_result = addition(num1, num2)
+    print(f"La somme de {num1} et {num2} est égale à : {sum_result}")
